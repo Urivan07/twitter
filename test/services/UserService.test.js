bdfe6a1f5e5a60e9f26967987cf1089479a1649f
Urivan <urivan07@gmail.com>
2022-05-02 00:15:48 -0500

    Add - metodo getInfo el cual retorna una lista con la informacion del usuario y se agrega prueba correspondiente

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,4 +9,14 @@ describe("Test for UserService", () => {
         expect(user.name).toBe("Urivan")
         expect(user.bio).not.toBeUndefined()
     });
+    test("2. Get all user data in list", () =>{
+        const user = UserService.create(1,"Urivan07", "Urivan")
+        const userInfoList = UserService.getInfo(user)
+
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("Urivan07")
+        expect(userInfoList[2]).toBe("Urivan")
+        expect(userInfoList[3]).toBe("Sin bio")
+
+    });
 });
